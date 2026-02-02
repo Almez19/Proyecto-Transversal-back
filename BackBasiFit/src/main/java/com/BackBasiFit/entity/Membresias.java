38ed6ffb3cc86a90caead443019edd0f0a522e3d
COMPU-S038\daw2 <pablo.rivero.gutierrez@gmail.com>
2026-02-02 19:55:18 +0100

    commit entitys hecho

--- BackBasiFit/src/main/java/com/BackBasiFit/entity/Membresias.java
+++ BackBasiFit/src/main/java/com/BackBasiFit/entity/Membresias.java
@@ -27,11 +27,115 @@ public class Membresias {
     @Column(nullable= false)
     boolean estado;
 
+    @Column(nullable= false)
+    Duracion duracion;
+
+    public enum Duracion{
+        diario("diario"),
+        semanal("semanal"),
+        mensual("mensual"),
+        trimestral("trimestral"),
+        anual("anual");
+    
+        private final String duracion;
+
+        Duracion(String duracion){
+            this.duracion = duracion;
+        }
+
+        public String getDuracion(){
+            return duracion;
+        }
+    }
+
+    @Column(nullable= false)
+    Calidad calidad;
 
 
+    public enum Calidad{
+        comfort("comfort"),
+        premium("premium"),
+        ultimate("ultimate");
+    
+        private final String calidad;
 
+        Calidad(String calidad){
+            this.calidad = calidad;
+        }
 
+        public String getCalidad(){
+            return calidad;
+        }
+    }
 
+    @Column(nullable= false)
+    Number precio;
 
+    @Column(nullable= false)
+    String usuario;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFecha_inicio() {
+        return fecha_inicio;
+    }
+
+    public void setFecha_inicio(String fecha_inicio) {
+        this.fecha_inicio = fecha_inicio;
+    }
+
+    public String getFecha_final() {
+        return fecha_final;
+    }
+
+    public void setFecha_final(String fecha_final) {
+        this.fecha_final = fecha_final;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public Duracion getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(Duracion duracion) {
+        this.duracion = duracion;
+    }
+
+    public Calidad getCalidad() {
+        return calidad;
+    }
+
+    public void setCalidad(Calidad calidad) {
+        this.calidad = calidad;
+    }
+
+    public Number getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Number precio) {
+        this.precio = precio;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
 
 }
