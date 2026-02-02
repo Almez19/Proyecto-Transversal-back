e17c3cfae6eb9529b8e37e98e4ac6da8c3825c85
COMPUS036\daw2\Documents\Clase <beniaminpatras@gmail.com>
2026-02-02 15:23:35 +0100

    Proyecto Sprengboot

---
+++ BackBasiFit/src/main/java/com/BackBasiFit/BackBasiFitApplication.java
@@ -0,0 +1,13 @@
+package com.BackBasiFit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackBasiFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackBasiFitApplication.class, args);
+	}
+
+}
---
+++ BackBasiFit/src/test/java/com/BackBasiFit/BackBasiFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BackBasiFit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackBasiFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
